8e971dff35b6912b7a46155e20ae59c92cf5de9a
“bditicom” <bditi.com@gmail.com>
2021-05-01 02:37:10 -0700

    class_5

---
+++ test.py
@@ -0,0 +1,16 @@
+#s=60
+#s=s*9
+#l=int(input())
+#print(s*l)
+
+#a=int(input("Total Step" ))
+#step=int(input("Frist Step"))
+#back=int(input("Re back "))
+#cal=(a/step)*back
+#print(int(cal))
+
+a=int(input("amar girlfriend number"))
+b=int(input("Girlfriend der total boyfriend ami soho"))
+c=b-1
+d=c/a
+print("proteker boyfriend number ami bade ",int(d))
